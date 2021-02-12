d9a91451d2a6599d55b4fcc7868b9ff323cbcdfd
fred milloh <fred.milloh@hotmail.com>
2021-02-12 11:50:45 +0100

    Add document

--- Sources/FrameworkCustomGreenColor/FrameworkCustomGreenColor.swift
+++ Sources/FrameworkCustomGreenColor/FrameworkCustomGreenColor.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 @available(iOS 13.0, *)
-extension UIColor{
+public extension UIColor{
+    /// Create a modified green color
+    /// - Returns: A modified green color
     class func getCustomGreen() -> UIColor{
         return UIColor(displayP3Red: 180/255, green: 213/255, blue: 21/255, alpha: 1.0)
     }
